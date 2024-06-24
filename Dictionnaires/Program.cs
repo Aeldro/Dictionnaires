462f3e2f890b6e0ac6a2fa1f2447a6f8e643de42
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-24 10:06:14 +0200

    Ajoutez des fichiers projet.

---
+++ Dictionnaires/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Dictionnaires
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, string> dico = new Dictionary<char, string>();
+            dico['a'] = "Je";
+            dico['b'] = "suis";
+            dico['c'] = "une string";
+            dico.Remove('c');
+        }
+    }
+}
